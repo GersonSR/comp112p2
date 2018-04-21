9bb272bd510020d1fe68799b32e4219433ffcc80
GersonSR <salmeron.gerson@gmail.com>
2018-04-21 14:41:58 -0400

    Pirates over Tanks.

--- server.js
+++ server.js
@@ -1,113 +1,90 @@
-/**
- * This is the server app script that is run on the server.
- * @author alvin.lin.dev@gmail.com (Alvin Lin)
- * TODO: Add unit tests!
- */
-
-var DEV_MODE = false;
-var PORT = process.env.PORT || 5000;
-var FRAME_RATE = 1000.0 / 60.0;
-
-/**
- * Sets the DEV_MODE constant for development.
- * Example usage:
- * node server.js --dev
- */
-process.argv.forEach(function(value, index, array) {
-  if (value == '--dev' || value == '--development') {
-    DEV_MODE = true;
-  }
-});
-
-// Dependencies.
-var bodyParser = require('body-parser');
-var express = require('express');
-var http = require('http');
-var morgan = require('morgan');
-var socketIO = require('socket.io');
-var swig = require('swig');
-
-var Game = require('./lib/Game');
-
-// Initialization.
+var express = require('express'); // Express contains some boilerplate to for routing and such
 var app = express();
-var server = http.Server(app);
-var io = socketIO(server);
-var game = new Game();
+var http = require('http').Server(app);
+var io = require('socket.io')(http); // Here's where we include socket.io as a node module 
 
-app.engine('html', swig.renderFile);
-
-app.set('port', PORT);
-app.set('view engine', 'html');
-
-app.use(morgan('dev'));
-app.use('/public', express.static(__dirname + '/public'));
-app.use('/shared', express.static(__dirname + '/shared'));
-
-// Routing
-app.get('/', function(request, response) {
-  response.render('index.html', {
-    dev_mode: DEV_MODE
-  });
+// Serve the index page 
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/index.html'); 
 });
 
-// Server side input handler, modifies the state of the players and the
-// game based on the input it receives. Everything runs asynchronously with
-// the game loop.
-io.on('connection', function(socket) {
-  // When a new player joins, the server adds a new player to the game.
-  socket.on('new-player', function(data, callback) {
-    game.addNewPlayer(data.name, socket);
-    io.sockets.emit('chat-server-to-clients', {
-      name: '[Tank Anarchy]',
-      message: data.name + ' has joined the game.',
-      isNotification: true
-    });
-    callback();
-    socket.emit('chat-server-to-clients', {
-      name: '[Tank Anarchy]',
-      message: 'Welcome, ' + data.name + '! Use WASD to move and click ' +
-          'to shoot. Pick up powerups to boost your tank temporarily!',
-      isNotification: true
-    });
-  });
+// Serve the assets directory
+app.use('/assets',express.static('assets'))
 
-  // Update the internal object states every time a player sends an intent
-  // packet.
-  socket.on('player-action', function(data) {
-    game.updatePlayer(socket.id, data.keyboardState, data.turretAngle,
-                      data.shot, data.timestamp);
-  });
-
-  socket.on('chat-client-to-server', function(data) {
-    io.sockets.emit('chat-server-to-clients', {
-      name: game.getPlayerNameBySocketId(socket.id),
-      message: data
-    });
-  });
-
-  // When a player disconnects, remove them from the game.
-  socket.on('disconnect', function() {
-    var name = game.removePlayer(socket.id);
-    io.sockets.emit('chat-server-to-clients', {
-      name: '[Tank Anarchy]',
-      message: name + ' has left the game.',
-      isNotification: true
-    });
-  });
+// Listen on port 5000
+app.set('port', (process.env.PORT || 5000));
+http.listen(app.get('port'), function(){
+  console.log('listening on port',app.get('port'));
 });
 
-// Server side game loop, runs at 60Hz and sends out update packets to all
-// clients every tick.
-setInterval(function() {
-  game.update();
-  game.sendState();
-}, FRAME_RATE);
+var players = {}; //Keeps a table of all players, the key is the socket id
+var bullet_array = []; // Keeps track of all the bullets to update them on the server 
+// Tell Socket.io to start accepting connections
+io.on('connection', function(socket){
+	// Listen for a new player trying to connect
+	socket.on('new-player',function(state){
+		console.log("New player joined with state:",state);
+		players[socket.id] = state;
+		// Broadcast a signal to everyone containing the updated players list
+		io.emit('update-players',players);
+	})
+  
+  // Listen for a disconnection and update our player table 
+  socket.on('disconnect',function(state){
+    delete players[socket.id];
+    io.emit('update-players',players);
+  }) 
+  
+  // Listen for move events and tell all other clients that something has moved 
+  socket.on('move-player',function(position_data){
+    if(players[socket.id] == undefined) return; // Happens if the server restarts and a client is still connected 
+    players[socket.id].x = position_data.x;  
+    players[socket.id].y = position_data.y; 
+    players[socket.id].angle = position_data.angle; 
+    io.emit('update-players',players);
+  })
+  
+  // Listen for shoot-bullet events and add it to our bullet array
+  socket.on('shoot-bullet',function(data){
+    if(players[socket.id] == undefined) return;
+    var new_bullet = data;
+    data.owner_id = socket.id; // Attach id of the player to the bullet 
+    if(Math.abs(data.speed_x) > 20 || Math.abs(data.speed_y) > 20){
+      console.log("Player",socket.id,"is cheating!");
+    }
+    bullet_array.push(new_bullet);
+  });
+})
 
-// Starts the server.
-server.listen(PORT, function() {
-  console.log('Starting server on port ' + PORT);
-  if (DEV_MODE) {
-    console.log('DEVELOPMENT MODE ENABLED: SERVING UNCOMPILED JAVASCRIPT!');
+// Update the bullets 60 times per frame and send updates 
+function ServerGameLoop(){
+  for(var i=0;i<bullet_array.length;i++){
+    var bullet = bullet_array[i];
+    bullet.x += bullet.speed_x; 
+    bullet.y += bullet.speed_y; 
+    
+    // Check if this bullet is close enough to hit any player 
+    for(var id in players){
+      if(bullet.owner_id != id){
+        // And your own bullet shouldn't kill you
+        var dx = players[id].x - bullet.x; 
+        var dy = players[id].y - bullet.y;
+        var dist = Math.sqrt(dx * dx + dy * dy);
+        if(dist < 70){
+          io.emit('player-hit',id); // Tell everyone this player got hit
+        }
+      }
+    }
+    
+    // Remove if it goes too far off screen 
+    if(bullet.x < -10 || bullet.x > 1000 || bullet.y < -10 || bullet.y > 1000){
+        bullet_array.splice(i,1);
+        i--;
+    }
+        
   }
-});
+  // Tell everyone where all the bullets are by sending the whole array
+  io.emit("bullets-update",bullet_array);
+}
+
+setInterval(ServerGameLoop, 16); 
