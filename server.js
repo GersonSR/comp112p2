987748e73c3ebe815b23857fc900d05d8f3afa65
Daniel Jin <danjin250@gmail.com>
2018-04-22 19:03:22 -0400

    Players get a random ship type. Removed console.log on player hit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express'); // Express contains some boilerplate to for routing and such
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http); // Here's where we include socket.io as a node module 
+var io = require('socket.io')(http); // Here's where we include socket.io as a node module
 
-// Serve the index page 
+// Serve the index page
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/index.html'); 
+  response.sendFile(__dirname + '/index.html');
 });
 
 // Serve the assets directory
@@ -18,7 +18,7 @@ http.listen(app.get('port'), function(){
 });
 
 var players = {}; //Keeps a table of all players, the key is the socket id
-var bullet_array = []; // Keeps track of all the bullets to update them on the server 
+var bullet_array = []; // Keeps track of all the bullets to update them on the server
 // Tell Socket.io to start accepting connections
 io.on('connection', function(socket){
 	// Listen for a new player trying to connect
@@ -28,27 +28,27 @@ io.on('connection', function(socket){
 		// Broadcast a signal to everyone containing the updated players list
 		io.emit('update-players',players);
 	})
-  
-  // Listen for a disconnection and update our player table 
+
+  // Listen for a disconnection and update our player table
   socket.on('disconnect',function(state){
     delete players[socket.id];
     io.emit('update-players',players);
-  }) 
-  
-  // Listen for move events and tell all other clients that something has moved 
-  socket.on('move-player',function(position_data){
-    if(players[socket.id] == undefined) return; // Happens if the server restarts and a client is still connected 
-    players[socket.id].x = position_data.x;  
-    players[socket.id].y = position_data.y; 
-    players[socket.id].angle = position_data.angle; 
-    io.emit('update-players',players);
   })
-  
+
+  // Listen for move events and tell all other clients that something has moved
+  socket.on('move-player',function(position_data){
+    if(players[socket.id] == undefined) return; // Happens if the server restarts and a client is still connected
+    players[socket.id].x = position_data.x;
+    players[socket.id].y = position_data.y;
+    players[socket.id].angle = position_data.angle;
+    io.emit('update-players', players);
+  })
+
   // Listen for shoot-bullet events and add it to our bullet array
   socket.on('shoot-bullet',function(data){
     if(players[socket.id] == undefined) return;
     var new_bullet = data;
-    data.owner_id = socket.id; // Attach id of the player to the bullet 
+    data.owner_id = socket.id; // Attach id of the player to the bullet
     data.hits = {}; //Array of ship IDs hit by the bullet
     if(Math.abs(data.speed_x) > 20 || Math.abs(data.speed_y) > 20){
       console.log("Player",socket.id,"is cheating!");
@@ -57,14 +57,14 @@ io.on('connection', function(socket){
   });
 })
 
-// Update the bullets 60 times per frame and send updates 
+// Update the bullets 60 times per frame and send updates
 function ServerGameLoop(){
   for(var i=0;i<bullet_array.length;i++){
     var bullet = bullet_array[i];
-    bullet.x += bullet.speed_x; 
-    bullet.y += bullet.speed_y; 
-    
-    // Check if this bullet is close enough to hit any player 
+    bullet.x += bullet.speed_x;
+    bullet.y += bullet.speed_y;
+
+    // Check if this bullet is close enough to hit any player
     for(var id in players){
       if(bullet.owner_id != id){
         // And your own bullet shouldn't kill you
@@ -75,7 +75,6 @@ function ServerGameLoop(){
           if(players[id].health > 1 && bullet.hits[id] == undefined) {
           	players[id].health -= 1;
           	bullet.hits[id] = true;
-          	console.log(players[id].health);
           	io.emit('player-hit',id); // Tell everyone this player got hit
           } else if (players[id].health <= 1 && bullet.hits[id] != undefined){
          	console.log(players[id].health);
@@ -84,16 +83,16 @@ function ServerGameLoop(){
         }
       }
     }
-    
-    // Remove if it goes too far off screen 
+
+    // Remove if it goes too far off screen
     if(bullet.x < -10 || bullet.x > 1000 || bullet.y < -10 || bullet.y > 1000){
         bullet_array.splice(i,1);
         i--;
     }
-        
+
   }
   // Tell everyone where all the bullets are by sending the whole array
   io.emit("bullets-update",bullet_array);
 }
 
-setInterval(ServerGameLoop, 16); 
+setInterval(ServerGameLoop, 16);
